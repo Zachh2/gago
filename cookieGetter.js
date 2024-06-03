04b80005147367ab5abe8bda4e4dad2596227e84
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-06-04 01:21:12 +0800

    Update cookieGetter.js

--- cookieGetter.js
+++ cookieGetter.js
@@ -1,4 +1,3 @@
-// WARNING: PLEASE DO NOT MODIFY IF YOU DONT WANT ANY PROBLEMS
 export async function getCookie(email, password, port) {
   try {
     const response = await fetch(`http://127.0.0.1:${port}/api/getCookie?email=${email}&password=${password}`);
@@ -6,10 +5,9 @@ export async function getCookie(email, password, port) {
     if (data.error) {
       return "Invalid Email/ID/Number or Password";
     }
-    const cookie = data.cookie.replace(/=\s+/g, '=');
+    const cookie = data.cookie.replace(/\s*=\s*/g, '=');
     return cookie;
   } catch (error) {
     return error;
   }
 }
-
