eb8082491c60383187489400312f172daf4ae879
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-06-04 22:00:27 +0800

    sasasawawawadaw dsaweaw

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 import requests
 import json
 
-app = Flask(_name_)
+app = Flask('_name_')
 
 def get_ua():
     return f"Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S{random.randrange(100, 9999)}/{random.randrange(100, 9999)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}{random.randrange(1, 9)}; U; Bada/1.2; en-us) AppleWebKit/533.1 (KHTML, like Gecko) Dolfin/{random.randrange(1, 9)}.{random.randrange(1, 9)} Mobile WVGA SMM-MMS/1.2.0 OPN-B"
